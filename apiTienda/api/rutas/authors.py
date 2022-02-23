ace15fbf4cba39d1bd251fce2154562a3e7bb541
Piero <matematicadivina1@gmail.com>
2022-02-23 16:28:07 -0500

    endpoint authors

--- apiTienda/api/rutas/authors.py
+++ apiTienda/api/rutas/authors.py
@@ -12,6 +12,7 @@ author_routes = Blueprint("author_routes", __name__)
 def create_author():
     try:
         data = request.get_json()
+        print(data)
         author_schema = AuthorSchema()
         author = Author(**data).create()
         #author, error = author_schema.load(data)
@@ -21,3 +22,18 @@ def create_author():
         return response_with(resp.SUCCESS_201, value={"author": result})
     except Exception as e:
         return response_with(resp.INVALID_INPUT_422)
+
+@author_routes.route('/', methods=['GET'])
+def get_author_list():
+
+    fetched = Author.query.all()
+    author_schema = AuthorSchema(many=True, only=['first_name', 'last_name', 'id', 'books'])
+    authors= author_schema.dump(fetched)
+    return response_with(resp.SUCCESS_200, value={"authors":authors})
+
+@author_routes.route('/<string:author_name>', methods=['GET'])
+def get_author_detail(author_name):
+    fetched = Author.query.filter_by(first_name=author_name).all()
+    author_schema =AuthorSchema(many = True)
+    author = author_schema.dump(fetched)
+    return response_with(resp.SUCCESS_200, value={"authors":author})
